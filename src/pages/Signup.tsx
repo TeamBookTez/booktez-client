ef1f0fe9d1d44345957e9acd8bba340b81068f42
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-01-09 02:46:58 +0900

    conflict 해결

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,15 +1,19 @@
-export default function Signup() {
+import styled from "styled-components";
+
+import { Logo } from "./../assets/imgs/index";
+
+export default function SignUp() {
   return (
     <>
-      <header>
+      <StHeader>
         <a>
-          <img alt="" />
-          북스테어즈
+          <img src={Logo} alt="홈페이지 로고 입니다. 클릭시 메인 페이지로 이동합니다" />
+          <p>북스테어즈</p>
         </a>
-      </header>
-      <main>
-        <article>
-          <div>
+      </StHeader>
+      <StMain>
+        <StArticle>
+          <StStep>
             <div>1</div>
             <div></div>
             <div></div>
@@ -17,7 +21,7 @@ export default function Signup() {
             <div></div>
             <div></div>
             <div>5</div>
-          </div>
+          </StStep>
           <h1>나만의 서재를 만드는 중이에요!</h1>
           <h2>제가 여러분을 어떻게 부르면 될까요?</h2>
           <form>
@@ -31,8 +35,88 @@ export default function Signup() {
             <p>비밀번호가 다릅니다</p>
             <button>다음 단계</button>
           </form>
-        </article>
-      </main>
+        </StArticle>
+      </StMain>
     </>
   );
 }
+
+const StHeader = styled.header`
+  position: absolute;
+  height: 9rem;
+  display: flex;
+  align-items: center;
+
+  & > a {
+    height: 3.6rem;
+    margin-left: 2rem;
+
+    align-items: center;
+    display: flex;
+  }
+
+  & > a:hover {
+    cursor: pointer;
+  }
+
+  & > a > img {
+    height: 100%;
+    width: auto;
+    margin-right: 0.8rem;
+  }
+
+  & > a > p {
+    text-align: center;
+
+    /* 임의글꼴 */
+    font-weight: 600;
+    font-size: 1.6rem;
+    text-align: center;
+  }
+`;
+
+const StMain = styled.main`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StArticle = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & > form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const StStep = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > div {
+    background-color: ${({ theme }) => theme.colors.orange100};
+  }
+
+  & > div:nth-child(3n + 1) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 4.3rem;
+    height: 4.3rem;
+    color: ${({ theme }) => theme.colors.white};
+    border-radius: 50%;
+
+    /* 임의 폰트 */
+    font-size: 1.8rem;
+  }
+
+  & > div:not(:nth-child(3n + 1)) {
+    width: 2.9rem;
+    height: 0.4rem;
+  }
+`;
