08b18544697d4f4961c8f9260379cb4e8dbe30a8
soryeongk <soryeongk.kr@gmail.com>
2022-01-10 03:45:41 +0900

    feat: add navigation icons

--- src/components/common/NavWrapper.tsx
+++ src/components/common/NavWrapper.tsx
@@ -1,28 +1,39 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+import { IcBookcase, IcHome, IcMyPage, IcToBe } from "../../assets/icons";
 import { NavHeader } from ".";
 
 export default function NavWrapper() {
   return (
     <StSection>
       <NavHeader />
-      <nav>
-        <ul>
-          <li>
-            <Link to="">홈</Link>
-          </li>
-          <li>
-            <Link to="">서재</Link>
-          </li>
-          <li>
-            <Link to="">마이페이지</Link>
-          </li>
-          <li>
-            <Link to="">곧 만나요</Link>
-          </li>
-        </ul>
-      </nav>
+      <StNav>
+        <StUl>
+          <StItem>
+            <StLink to="">
+              <IcHome />홈
+            </StLink>
+          </StItem>
+          <StItem>
+            <StLink to="">
+              <IcBookcase />
+              서재
+            </StLink>
+          </StItem>
+          <StItem>
+            <StLink to="">
+              <IcMyPage />
+              마이페이지
+            </StLink>
+          </StItem>
+          <StItem>
+            <StLink to="">
+              <IcToBe />곧 만나요
+            </StLink>
+          </StItem>
+        </StUl>
+      </StNav>
     </StSection>
   );
 }
@@ -30,4 +41,33 @@ export default function NavWrapper() {
 const StSection = styled.section`
   position: relative;
   width: 17.5rem;
+  color: ${({ theme }) => theme.colors.white500};
+`;
+
+const StNav = styled.nav`
+  margin-top: 12.3rem;
+  padding-left: 2.2rem;
+  font-size: 1.6rem;
+  font-weight: bold;
+  line-height: 1.9rem;
+`;
+
+const StUl = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 2.8rem;
+`;
+
+const StItem = styled.li`
+  display: flex;
+  align-items: center;
+`;
+
+const StLink = styled(Link)`
+  display: flex;
+  align-items: center;
+
+  & > svg {
+    margin-right: 1rem;
+  }
 `;
