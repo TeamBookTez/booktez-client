a3dc519bfc639456aab8df40b2656f837dbb41a2
Bebop <tjr50999@naver.com>
2022-01-09 20:09:32 +0900

    feat: common button component 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,3 +1,25 @@
-export default function Button() {
-  return <div>Button</div>;
+import styled from "styled-components";
+
+interface ButtonProps {
+  children: string;
 }
+
+export default function Button(props: ButtonProps) {
+  const children = props.children;
+
+  return <StButton>{children}</StButton>;
+}
+
+const StButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 12.1rem;
+  height: 3.2rem;
+  border-radius: 2.1rem;
+  background-color: ${({ theme }) => theme.colors.orange100};
+
+  font-size: 1.6rem;
+  letter-spacing: -0.1rem;
+  color: ${({ theme }) => theme.colors.white};
+`;
