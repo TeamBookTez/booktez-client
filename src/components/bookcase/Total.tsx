345bb3df5a39a3e3fedc8d09e46174fd50acfed8
Bebop <tjr50999@naver.com>
2022-01-09 20:16:13 +0900

    feat: 서재 책 없을때 뷰 구현

--- src/components/bookcase/Total.tsx
+++ src/components/bookcase/Total.tsx
@@ -1,17 +1,54 @@
+import styled from "styled-components";
+
+import Button from "../../components/common/Button";
+
 export default function Total() {
   return (
-    <section>
-      <article>
-        <img alt="아이콘" />
-        <h3>다음 책을 쌓아볼까요?</h3>
-        <h4>이정도면 좋겠니아ㅣㅏㄹ</h4>
-        <button>+ 책 추가</button>
-      </article>
-      <article>
-        <img alt="책 표지 이미지" />
-        <h4>나는 왜 이 일을 하는가?</h4>
-        <h5>데이빗 호킨스</h5>
-      </article>
-    </section>
+    <StSection>
+      <StCard>
+        <img src={require("../../assets/imgs/notbooksimg.png")} alt="아이콘" />
+        <StCardHeader>아직 읽은 책이 없어요</StCardHeader>
+        <StCardDesc>이정도면 좋겠니아ㅣㅏㄹ</StCardDesc>
+        <Button>+ 책 추가</Button>
+      </StCard>
+    </StSection>
   );
 }
+
+const StSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 83%;
+`;
+
+const StCard = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & > img {
+    margin-bottom: 1.7rem;
+  }
+`;
+
+const StCardHeader = styled.h3`
+  margin-bottom: 1.1rem;
+
+  font-size: 2rem;
+  font-weight: 700;
+  line-height: 2.6rem;
+  letter-spacing: -0.1rem;
+  color: ${({ theme }) => theme.colors.gray300};
+`;
+
+const StCardDesc = styled.p`
+  margin-bottom: 2.4rem;
+
+  font-size: 1.6rem;
+  font-weight: 400;
+  line-height: 2rem;
+  letter-spacing: -0.1rem;
+  color: ${({ theme }) => theme.colors.gray300};
+`;
