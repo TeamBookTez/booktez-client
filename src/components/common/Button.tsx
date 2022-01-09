6a28b822101de37bf0876047e73b283c0dd493de
soryeongk <soryeongk.kr@gmail.com>
2022-01-10 04:31:26 +0900

    refactor: button style 재사용

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,3 +1,7 @@
-export default function Button() {
-  return <div>Button</div>;
-}
+import styled from "styled-components";
+
+export const Button = styled.button`
+  border-radius: 2.1rem;
+  background-color: ${({ theme }) => theme.colors.orange100};
+  color: ${({ theme }) => theme.colors.white};
+`;
