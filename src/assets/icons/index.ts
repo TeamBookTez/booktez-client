da5b9774efdd5e4797de28bccdfd4786a6f89728
soryeongk <soryeongk.kr@gmail.com>
2022-01-10 03:23:51 +0900

    init: add icon assets

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,6 @@
+export { ReactComponent as IcBookcase } from "./bookcaseIcon.svg";
 export { ReactComponent as IcCool } from "./coolicon.svg";
+export { ReactComponent as IcHome } from "./homeIcon.svg";
 export { ReactComponent as IcLogo } from "./logoicon.svg";
+export { ReactComponent as IcMyPage } from "./myPageIcon.svg";
+export { ReactComponent as IcToBe } from "./toBeIcon.svg";
