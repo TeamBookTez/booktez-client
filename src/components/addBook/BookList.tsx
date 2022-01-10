33467a9b60ca752b7735f63dde8e07478a053ca8
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-01-10 19:11:44 +0900

    feat: BookInfoWrapper 안에 article 구성

--- src/components/addBook/BookList.tsx
+++ src/components/addBook/BookList.tsx
@@ -1,3 +1,16 @@
+import styled from "styled-components";
+
+import BookInfoWrapper from "./BookInfoWrapper";
+
 export default function BookList() {
-  return <div></div>;
+  return (
+    <StListWrapper>
+      {/* map으로 여러개 불러오기 */}
+      <BookInfoWrapper />
+    </StListWrapper>
+  );
 }
+
+const StListWrapper = styled.section`
+  margin: 6.4rem 12.1rem 2.9rem 5.6rem;
+`;
