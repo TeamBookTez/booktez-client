07ef1abda6dff15df42b62020a25cb03be71005b
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-01-10 15:25:58 +0900

    fix: assets 폴더 교체

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,5 @@
 export { default as ImgAddBooks } from "./addBooksImage.png";
 export { default as ImgEmptyBooks } from "./emptyBooksImage.png";
 export { default as ImgGraphic } from "./graphicImage.png";
-export { default as ImgLogo } from "./logoImage.png";
+export { default as ImgLogo } from "./LogoImage.png";
 export { default as ImgTemp } from "./tempImage.png";
