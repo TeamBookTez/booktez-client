f00b186ce2b08d5b130ad82f12d2d89eefe7d899
soryeongk <soryeongk.kr@gmail.com>
2022-01-13 13:57:37 +0900

    feat: main 하단 html 구조 짜기

---
+++ src/components/main/Recent.tsx
@@ -0,0 +1,11 @@
+export default function Recent() {
+  return (
+    <section>
+      <header>
+        <h3>최근 펼쳐본 책이에요!</h3>
+        <button>전체보기</button>
+      </header>
+      <section>blah</section>
+    </section>
+  );
+}
