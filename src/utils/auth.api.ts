82312cadeb69190c6e1388ad3c614ea271cf221d
soryeongk <soryeongk.kr@gmail.com>
2022-01-14 18:11:49 +0900

    fix: auth api 연결

--- src/utils/auth.api.ts
+++ src/utils/auth.api.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export const loginAxios = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URI}`,
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
@@ -24,9 +24,9 @@ export const postSignUp = async (SignUpData: SignUp): Promise<boolean> => {
     if (data !== undefined) {
       if (data.data.status === 201) return true;
     }
-  } catch (e: any) {
-    if (e.response !== undefined) {
-      alert(e.response.data.message);
+  } catch (error) {
+    if (error instanceof Error) {
+      alert(error.message);
     }
   }
 
