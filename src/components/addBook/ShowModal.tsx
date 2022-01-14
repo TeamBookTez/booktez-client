dc53f38da29289105fe5ac635f0acb3e17f98228
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-01-14 09:29:41 +0900

    fix: 필요 없는 import 삭제

--- src/components/addBook/ShowModal.tsx
+++ src/components/addBook/ShowModal.tsx
@@ -1,8 +1,6 @@
-import React from "react";
 import styled from "styled-components";
 
 import { ImgNull } from "../../assets/images";
-import { BookInfo } from "../../pages/AddBook";
 import { PublishDate } from "./BookInfoWrapper";
 
 interface ShowModalProps {
