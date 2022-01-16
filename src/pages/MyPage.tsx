1e9e432983089ed3cf8e0895f123de99187f8212
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-01-16 17:22:35 +0900

    feat: myInfo API 통신 완료

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,8 +1,38 @@
+import axios, { AxiosRequestHeaders } from "axios";
+import { emitWarning } from "process";
+import { useEffect } from "react";
+
 import { MainHeader } from "../components/common";
 import BottomContent from "../components/mypage/BottomContent";
 import TopContent from "../components/mypage/TopContent";
+import { getData } from "../utils/lib/api";
 
 export default function MyPage() {
+  const infoHeaders: AxiosRequestHeaders = {
+    "Content-Type": "application/json",
+    Authorization: `${process.env.REACT_APP_TEST_TOKEN}`,
+  };
+
+  const infoKey = "/user/myInfo";
+
+  const getInfo = async (headers: AxiosRequestHeaders, key: string) => {
+    try {
+      const { data } = await getData(headers, key);
+
+      const { email, img, nickname, reviewCount } = data.data;
+
+      console.log(email, img, nickname, reviewCount);
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        console.log("err", err.response?.data);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getInfo(infoHeaders, infoKey);
+  }, []);
+
   return (
     <>
       <MainHeader>마이페이지</MainHeader>
