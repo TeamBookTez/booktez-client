36a3f850ea72df89cb0828b8da33554ef716969b
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-01-18 02:22:07 +0900

    feat: get을 통해서 꼬리 질문에 대답을 가져와 보는 중

--- src/components/bookNote/perinote/QuestionForm.tsx
+++ src/components/bookNote/perinote/QuestionForm.tsx
@@ -9,18 +9,28 @@ export default function QuestionForm(props: QuestionFormInfo) {
   const { formList } = props;
   const { question, answer, depth } = formList;
 
-  console.log(answer);
+  const answerText = answer[0].text;
+  const answerChildren = answer[0].children;
+
+  console.log(answerChildren);
 
   return (
     <QuestionFormWrapper>
       <StQuestion>{question}</StQuestion>
-      <StAnswer></StAnswer>
+      <StAnswer>{answerText}</StAnswer>
+      {answerChildren.map((child: FormListInfo, idx: number) => (
+        <QuestionForm formList={child} key={idx} />
+      ))}
     </QuestionFormWrapper>
   );
 }
 
-const QuestionFormWrapper = styled.div``;
+const QuestionFormWrapper = styled.div`
+  width: 100%;
 
-const StQuestion = styled.label``;
+  ${({ theme }) => theme.fonts.header4};
+`;
 
-const StAnswer = styled.input``;
+const StQuestion = styled.div``;
+
+const StAnswer = styled.div``;
