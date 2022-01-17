0e6aae960f278a38313b10254602571cfd4a8744
Bebop <tjr50999@naver.com>
2022-01-17 17:16:10 +0900

    feat: 이메일 중복 검사 api 연결

--- src/utils/lib/auth.api.ts
+++ src/utils/lib/auth.api.ts
@@ -10,9 +10,9 @@ interface SignUp {
 
 const PREFIX_URL = "/auth";
 
-export const postSignup = async (body: SignUp) => {
+export const postSignup = async (signupData: SignUp) => {
   try {
-    const data = await client.post(`${PREFIX_URL}/signup`, body);
+    const data = await client.post(`${PREFIX_URL}/signup`, signupData);
 
     localStorage.setItem("token", data.data.token);
   } catch (err) {
@@ -20,4 +20,24 @@ export const postSignup = async (body: SignUp) => {
       console.log("err", err.response?.data);
     }
   }
+};
+
+export const getEmail = async (emailData: string) => {
+  try {
+    const { data: response } = await client.get(`${PREFIX_URL}/email?email=${emailData}`);
+
+    if (response !== undefined) {
+      if (response.status === 200) {
+        // console.log(response.message);
+
+        return response.message;
+      }
+    }
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      console.log("err", err.response?.data);
+    }
+  }
+
+  return console.log("딱 가져온나");
 };
