c174958096b8b4a17ca7216614c72b9893f577a8
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-01-19 22:58:26 +0900

    feat: 예시 상세페이지 버튼 삭제

--- src/pages/DetailExample.tsx
+++ src/pages/DetailExample.tsx
@@ -1,34 +1,21 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-import { IcDeleteNote, IcModifyNote } from "../assets/icons";
-import { PopUpDelete } from "../components/common";
 import { StBookTitle, StIcCancelWhite, StNoteModalWrapper } from "../components/common/styled/NoteModalWrapper";
 import { ExamplePeriNote, ExamplePreNote } from "../components/detail";
 import DetailArticleWrapper from "../components/detail/DetailArticleWrapper";
 import { reviewData } from "../utils/mockData";
 
 export default function DetailExample() {
-  const [isPopUp, setIsPopUp] = useState<boolean>(false);
-  const reviewId = 4; // 리뷰 id 를 받아와 처리
   const isLogin = false;
   // const _token = localStorage.getItem("booktez-token");
   const navigate = useNavigate();
 
-  const handlePopUp = () => {
-    setIsPopUp((isPopUp) => !isPopUp);
-  };
-
   return (
     <>
       <StNoteModalWrapper>
-        {isPopUp ? <></> : <StIcCancelWhite onClick={() => navigate(-1)} />}
-        <StBookTitle>{reviewData?.bookTitle}</StBookTitle>
-        <StBtnWrapper>
-          <IcDeleteNote onClick={handlePopUp} />
-          <IcModifyNote />
-        </StBtnWrapper>
+        <StIcCancelWhite onClick={() => navigate(-1)} />
+        <StBookTitleUp>{reviewData?.bookTitle}</StBookTitleUp>
         <DetailArticleWrapper title="독서 전 단계">
           <ExamplePreNote
             answerOne={reviewData?.answerOne}
@@ -45,22 +32,12 @@ export default function DetailExample() {
           </StMarginTop>
         )}
       </StNoteModalWrapper>
-      {isPopUp ? <PopUpDelete onPopUp={handlePopUp} reviewId={reviewId} /> : <></>}
     </>
   );
 }
 
-const StBtnWrapper = styled.div`
-  text-align: right;
-
-  & > svg {
-    &:hover {
-      cursor: pointer;
-    }
-  }
-  & > svg:not(:first-child) {
-    margin-left: 1.4rem;
-  }
+const StBookTitleUp = styled(StBookTitle)`
+  margin-bottom: 7.2rem;
 `;
 
 const StMarginTop = styled.div`
