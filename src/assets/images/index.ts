bdc319621b7a1c4d2ee18679ed70b0b9ef119023
Bebop <tjr50999@naver.com>
2022-01-21 04:07:04 +0900

    design: 삭제 팝업 svg 업데이트

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,6 @@
 export { default as ImgAddBooksSearch } from "./addBookSearchImage.png";
 export { default as ImgAddBooks } from "./addBooksImage.png";
+export { default as ImgdeletePopUp } from "./deleteImage.png";
 export { default as ImgDevice } from "./deviceImage.png";
 export { default as ImgEmptyBook } from "./emptyBookImage.png";
 export { default as ImgEmptyBooks } from "./emptyBooksImage.png";
