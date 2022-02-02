fcd610eca334559945b206a31c72522a3f6f78fb
Bebop <tjr50999@naver.com>
2022-02-02 15:35:38 +0900

    feat: divide bookcase post api

--- src/pages/Bookcase.tsx
+++ src/pages/Bookcase.tsx
@@ -15,7 +15,6 @@ export interface BookcaseInfo {
 
 export default function Bookcase() {
   const [bookcaseTotal, setBookcaseTotal] = useState<BookcaseInfo[]>([]);
-  const [bookcasePost, setBookcasePost] = useState<BookcaseInfo[]>([]);
   const [isLogin, setIsLogin] = useState<boolean>(true);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
@@ -74,7 +73,7 @@ export default function Bookcase() {
         <>
           <MainHeader>서재</MainHeader>
           <Navigation />
-          <Outlet context={[bookcaseTotal, bookcasePost, handleIsLoading, handleBookDelete, isLogin]} />
+          <Outlet context={[bookcaseTotal, handleIsLoading, handleBookDelete, isLogin]} />
         </>
       )}
     </>
