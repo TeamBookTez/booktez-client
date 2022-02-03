ea934fb73ab24fb1fa6e04fdb22e69129be24bc1
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-02-03 19:13:40 +0900

    fix: fix main layout

--- src/components/common/CommonLayout.tsx
+++ src/components/common/CommonLayout.tsx
@@ -17,5 +17,5 @@ export default function CommonLayout() {
 const StWrapper = styled.section`
   display: flex;
   background-color: ${({ theme }) => theme.colors.gray100};
-  height: 100vh;
+  min-height: 100vh;
 `;
--- src/components/common/MainLayout.tsx
+++ src/components/common/MainLayout.tsx
@@ -9,12 +9,13 @@ export default function MainLayout({ children }: MainLayoutProps) {
 }
 
 const StWrapper = styled.main`
+  margin-left: 17.5rem;
+
   position: relative;
   flex: 1;
   border-radius: 2rem 0 0 2rem;
   border: 0.1rem solid ${({ theme }) => theme.colors.white};
   background-color: ${({ theme }) => theme.colors.white};
 
-  font-family: pretendard;
   overflow-y: auto;
 `;
