a67b117e52b2de0b9615d1ec25d9d84dde34742a
Bebop <tjr50999@naver.com>
2022-02-11 17:16:42 +0900

    feat: link custom hook to bookcase components

--- src/components/bookcase/PeriRead.tsx
+++ src/components/bookcase/PeriRead.tsx
@@ -1,52 +1,16 @@
-import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 
-import { BookcaseInfo } from "../../pages/Bookcase";
-import { getMockData } from "../../utils/lib/api";
+import { useGetBookcase } from "../../pages/Bookcase";
 import { Loading } from "../common";
 import Cards from "./Cards";
 
 export default function PeriRead() {
-  const [isLoading, handleIsLoading, isLogin] = useOutletContext<[boolean, () => void, boolean]>();
-  const [bookcasePeri, setBookcasePeri] = useState<BookcaseInfo[]>([]);
-
-  const TOKEN = localStorage.getItem("booktez-token");
-  const localToken = TOKEN ? TOKEN : "";
-
-  const handleBookDelete = () => {
-    getBookcasePeri("/peribook", localToken);
-  };
-
-  useEffect(() => {
-    getBookcasePeri("/peribook", localToken);
-
-    return () => {
-      handleIsLoading();
-    };
-  }, []);
-
-  const getBookcasePeri = async (key: string, token: string) => {
-    try {
-      const {
-        data: {
-          data: { books },
-        },
-      } = await getMockData(key, token);
-
-      setBookcasePeri(books);
-    } catch (err) {
-      console.log("err", err);
-    }
-    handleIsLoading();
-  };
+  const isLogin = useOutletContext<boolean>();
+  const { bookcaseInfo, isLoading, getBookcase } = useGetBookcase("/book/peri");
 
   return (
     <>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <Cards bookcaseInfo={bookcasePeri} handleBookDelete={handleBookDelete} isLogin={isLogin} />
-      )}
+      {isLoading ? <Loading /> : <Cards bookcaseInfo={bookcaseInfo} handleBookDelete={getBookcase} isLogin={isLogin} />}
     </>
   );
 }
--- src/components/bookcase/PreRead.tsx
+++ src/components/bookcase/PreRead.tsx
@@ -1,52 +1,16 @@
-import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 
-import { BookcaseInfo } from "../../pages/Bookcase";
-import { getMockData } from "../../utils/lib/api";
+import { useGetBookcase } from "../../pages/Bookcase";
 import { Loading } from "../common";
 import Cards from "./Cards";
 
 export default function PreRead() {
-  const [isLoading, handleIsLoading, isLogin] = useOutletContext<[boolean, () => void, boolean]>();
-  const [bookcasePre, setBookcasePre] = useState<BookcaseInfo[]>([]);
-
-  const TOKEN = localStorage.getItem("booktez-token");
-  const localToken = TOKEN ? TOKEN : "";
-
-  const handleBookDelete = () => {
-    getBookcasePre("/prebook", localToken);
-  };
-
-  useEffect(() => {
-    getBookcasePre("/prebook", localToken);
-
-    return () => {
-      handleIsLoading();
-    };
-  }, []);
-
-  const getBookcasePre = async (key: string, token: string) => {
-    try {
-      const {
-        data: {
-          data: { books },
-        },
-      } = await getMockData(key, token);
-
-      setBookcasePre(books);
-    } catch (err) {
-      console.log("err", err);
-    }
-    handleIsLoading();
-  };
+  const isLogin = useOutletContext<boolean>();
+  const { bookcaseInfo, isLoading, getBookcase } = useGetBookcase("/book/pre");
 
   return (
     <>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <Cards bookcaseInfo={bookcasePre} handleBookDelete={handleBookDelete} isLogin={isLogin} />
-      )}
+      {isLoading ? <Loading /> : <Cards bookcaseInfo={bookcaseInfo} handleBookDelete={getBookcase} isLogin={isLogin} />}
     </>
   );
 }
