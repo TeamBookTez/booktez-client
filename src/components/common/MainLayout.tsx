8bf4b650517166e2a08c2453b693a35bf6db8a2a
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-02-13 20:11:40 +0900

    style: nav wrapper width marking

--- src/components/common/MainLayout.tsx
+++ src/components/common/MainLayout.tsx
@@ -10,10 +10,10 @@ export default function MainLayout({ children }: MainLayoutProps) {
 
 const StWrapper = styled.main`
   position: relative;
-  width: calc(100% - 17.5rem);
+  width: calc(100% - 17.5rem); //NAV_WRAPPER_WIDTH
   min-height: 100vh;
 
-  margin-left: 17.5rem;
+  margin-left: 17.5rem; //NAV_WRAPPER_WIDTH
 
   border-radius: 2rem 0 0 2rem;
   border: 0.1rem solid ${({ theme }) => theme.colors.white};
--- src/utils/atoms.ts
+++ src/utils/atoms.ts
@@ -4,8 +4,3 @@ export const isLoginState = atom<boolean>({
   key: "isLogin",
   default: false,
 });
-
-export const navWrapperWidthState = atom<number>({
-  key: "navWrapperWidth",
-  default: 17.5,
-});
