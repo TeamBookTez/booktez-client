bc2b72b3552bbcab93e261014329adc787151823
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-02-14 20:41:17 +0900

    feat: drawer wrapper component, arrow icon cursor pointer

--- src/components/bookNote/DrawerWrapper.tsx
+++ src/components/bookNote/DrawerWrapper.tsx
@@ -57,7 +57,7 @@ export default function DrawerWrapper(props: DrawerWrapperProps) {
           exit={{ transform: "translateX(39rem)" }}
           idx={idx}>
           <StIcWrapper>
-            <IcLeftArrow onClick={() => onCloseDrawer(idx)} />
+            <StIcLeftArrow onClick={() => onCloseDrawer(idx)} />
           </StIcWrapper>
           {idx === 4 ? <StImg src={ImgDrawer} idx={idx} /> : <StImg src={ImgDrawerSmall} idx={idx} />}
           <StTitleWrapper>
@@ -70,11 +70,6 @@ export default function DrawerWrapper(props: DrawerWrapperProps) {
     </AnimatePresence>
   );
 }
-
-const StIcWrapper = styled.div`
-  text-align: left;
-  margin-bottom: 3.2rem;
-`;
 
 const StDrawerWrapper = styled(motion.section)<StDrawerWrapperProps>`
   overflow-y: scroll;
@@ -104,6 +99,17 @@ const StDrawerWrapper = styled(motion.section)<StDrawerWrapperProps>`
     height: 4.4rem;
 
     margin-bottom: 3.2rem;
+  }
+`;
+
+const StIcWrapper = styled.div`
+  text-align: left;
+  margin-bottom: 3.2rem;
+`;
+
+const StIcLeftArrow = styled(IcLeftArrow)`
+  &:hover {
+    cursor: pointer;
   }
 `;
 
