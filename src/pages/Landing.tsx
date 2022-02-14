a363ea92be1cc6a2a5204c2d5528f2ca2c23556e
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-02-14 15:32:13 +0900

    bug: 비동기 상태 처리 error

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 
 import {
@@ -11,16 +12,20 @@ import {
   LandingThree,
   LandingTwo,
 } from "../components/landing";
+import { isLoginSelector } from "../utils/atoms";
 import { getData } from "../utils/lib/api";
 
 export default function Landing() {
   const navigate = useNavigate();
   const tempToken = localStorage.getItem("booktez-token");
   const localToken = tempToken ? tempToken : "";
+  const isLogin = useRecoilValue(isLoginSelector);
 
   const getLogin = async (key: string, token: string) => {
     try {
       const { data } = await getData(key, token);
+
+      console.log(key, token);
       const status = data.status;
 
       if (status === 200) {
@@ -34,7 +39,8 @@ export default function Landing() {
   };
 
   useEffect(() => {
-    getLogin("/auth/check", localToken);
+    // getLogin("/auth/check", localToken);
+    console.log(isLogin);
   }, []);
 
   return (
--- src/utils/atoms.ts
+++ src/utils/atoms.ts
@@ -1,6 +1,31 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+
+import { getData } from "./lib/api";
 
 export const isLoginState = atom<boolean>({
   key: "isLogin",
   default: false,
+});
+
+export const isLoginSelector = selector({
+  key: "isLoginSelector",
+  get: async () => {
+    try {
+      const { data } = await getData(
+        "/auth/check",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxM30sImlhdCI6MTY0NDgwNjM4OSwiZXhwIjoxNjQ2MDE1OTg5fQ.FfV1lVZ-tPY_bue10T1LOiGld9hLV3b-J4zXajg5b-0",
+      );
+      const status = data.status;
+
+      if (status === 200) {
+        if (data.data.isLogin === true) {
+          return true;
+        }
+      }
+
+      return false;
+    } catch (err) {
+      return false;
+    }
+  },
 });
