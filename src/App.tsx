36d2bc71cde3c96aace4b4fdc8179ad0a90e87bb
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-02-15 20:13:05 +0900

    etc: Suspense fallback UI 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { Suspense } from "react";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 
-import { Loading } from "./components/common";
 import Router from "./components/Router";
 import GlobalStyle from "./styles/globalStyle";
 import theme from "./styles/theme";
@@ -10,12 +8,10 @@ import theme from "./styles/theme";
 function App() {
   return (
     <RecoilRoot>
-      <Suspense fallback={<Loading />}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Router />
-        </ThemeProvider>
-      </Suspense>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
     </RecoilRoot>
   );
 }
