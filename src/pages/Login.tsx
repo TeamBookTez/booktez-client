44564ccff52a8b4ba09e45f8050bc1ff4c58da3d
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-02-15 19:42:42 +0900

    fix: Login page useHook 붙이기

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,46 +1,56 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 import styled, { css, keyframes } from "styled-components";
 
+import { Loading } from "../components/common";
 import { LoginForm, LoginNavSection } from "../components/login";
-import { isLoginSelector, isLoginState } from "../utils/atoms";
+import { isLoginState } from "../utils/atoms";
+import { useCheckLoginState } from "../utils/useHooks";
 
 export default function Login() {
   const [isAniTime, setIsAniTime] = useState<boolean>(false);
   const navigate = useNavigate();
-  const isLoginFromSelector = useRecoilValue(isLoginSelector);
+  const { isLogin, isLoginLoading } = useCheckLoginState();
   const setIsLogin = useSetRecoilState(isLoginState);
 
   useEffect(() => {
-    if (isLoginFromSelector) {
+    if (isLogin) {
       setIsLogin(true);
       navigate("/main");
     } else {
       setIsLogin(false);
     }
-  }, []);
+  }, [isLogin]);
 
   const handleAni = () => {
     setIsAniTime(true);
   };
 
   return (
-    <StPageWrapper>
-      <LoginNavSection isAniTime={isAniTime} onAniChange={handleAni} />
-      <StMainWrapper isAniTime={isAniTime}>
-        <StArticle>
-          <StH2>이미 서재가 있으신가요?</StH2>
-          <StH3>
-            북스테어즈에 로그인하고
-            <br />
-            서재에서 독서를 이어가세요.
-          </StH3>
-          <LoginForm />
-          <StContact>이메일/비밀번호를 잊어버리셨다면?</StContact>
-        </StArticle>
-      </StMainWrapper>
-    </StPageWrapper>
+    <>
+      {isLoginLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <StPageWrapper>
+            <LoginNavSection isAniTime={isAniTime} onAniChange={handleAni} />
+            <StMainWrapper isAniTime={isAniTime}>
+              <StArticle>
+                <StH2>이미 서재가 있으신가요?</StH2>
+                <StH3>
+                  북스테어즈에 로그인하고
+                  <br />
+                  서재에서 독서를 이어가세요.
+                </StH3>
+                <LoginForm />
+                <StContact>이메일/비밀번호를 잊어버리셨다면?</StContact>
+              </StArticle>
+            </StMainWrapper>
+          </StPageWrapper>
+        </>
+      )}
+    </>
   );
 }
 
