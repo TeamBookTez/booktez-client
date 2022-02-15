3915796ef84283f8ee9614a2c329649b42986cf7
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-02-15 18:06:42 +0900

    fix: <Recoil Selector>, 버리기로 결정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 
 import { MainHeader } from "../components/common";
 import { Banner, RecentBooks } from "../components/main";
 import { isLoginSelector, isLoginState } from "../utils/atoms";
+import { getData } from "../utils/lib/api";
+
+function useCheckLoginState() {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const tempToken = localStorage.getItem("booktez-token");
+  const TOKEN = tempToken ? tempToken : "";
+  const API_PATH = "/auth/check";
+
+  useEffect(() => {
+    checkLoginState();
+  }, []);
+
+  const checkLoginState = async () => {
+    try {
+      const { data } = await getData(API_PATH, TOKEN);
+      const status = data.status;
+
+      if (status === 200) {
+        if (data.data.isLogin === true) {
+          return setIsLogin(true);
+        }
+      }
+
+      setIsLogin(false);
+      setIsLoading(false);
+    } catch (err) {
+      setIsLogin(false);
+      setIsLoading(false);
+      throw err;
+    }
+  };
+
+  return { isLogin, isLoading };
+}
 
 export default function Main() {
-  const isLoginFromSelector = useRecoilValue(isLoginSelector);
+  const isLoginFromSelector = useCheckLoginState();
   const setIsLogin = useSetRecoilState(isLoginState);
 
   useEffect(() => {
