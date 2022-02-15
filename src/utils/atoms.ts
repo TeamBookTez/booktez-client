f77c93276e3f3fbbb71091792631af557d408c88
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-02-15 20:11:13 +0900

    etc: 애증의 selector 삭제

--- src/utils/atoms.ts
+++ src/utils/atoms.ts
@@ -1,32 +1,6 @@
-import { atom, selector } from "recoil";
-
-import { getData } from "./lib/api";
+import { atom } from "recoil";
 
 export const isLoginState = atom<boolean>({
-  key: "isLogin",
+  key: "isLoginState",
   default: false,
-});
-
-export const isLoginSelector = selector({
-  key: "isLoginSelector",
-  get: async () => {
-    const tempToken = localStorage.getItem("booktez-token");
-    const TOKEN = tempToken ? tempToken : "";
-    const API_PATH = "/auth/check";
-
-    try {
-      const { data } = await getData(API_PATH, TOKEN);
-      const status = data.status;
-
-      if (status === 200) {
-        if (data.data.isLogin === true) {
-          return true;
-        }
-      }
-
-      return false;
-    } catch (err) {
-      return false;
-    }
-  },
 });
