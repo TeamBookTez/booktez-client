832212b186a9c9b9106d93e49964b03e22f74cd1
soryeongk <soryeongk.kr@gmail.com>
2022-02-16 19:55:36 +0900

    feat: useSWR로 데이터 fetch 하기

--- src/components/bookcase/Cards.tsx
+++ src/components/bookcase/Cards.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import { BookcaseInfo } from "../../pages/Bookcase";
-import { getData } from "../../utils/lib/api";
-import { Loading } from "../common";
+import { getData, useGetBookInfo } from "../../utils/lib/api";
+import { Error404, Loading } from "../common";
 import { AddBookCard, BookCard } from ".";
 import Empty from "./cardSection/Empty";
 
@@ -12,58 +12,32 @@ interface CardsProps {
   navIndex: number;
 }
 
-const TOKEN = localStorage.getItem("booktez-token");
-const localToken = TOKEN ? TOKEN : "";
-
-const useGetBookcase = (key: string, navIndex: number) => {
-  const [bookcaseInfo, setBookcaseInfo] = useState<BookcaseInfo[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  const getBookcase = async () => {
-    try {
-      const {
-        data: {
-          data: { books },
-        },
-      } = await getData(key, localToken);
-
-      console.log("books", books);
-      setBookcaseInfo(books);
-    } catch (err) {
-      console.log("err", err);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    getBookcase();
-  }, [navIndex]);
-
-  return { bookcaseInfo, isLoading };
-};
-
 export default function Cards(props: CardsProps) {
   const { isLogin, navIndex } = props;
-  let path: string;
+  const [path, setPath] = useState<string>("");
 
-  switch (navIndex) {
-    case 1:
-      path = "/pre";
-      break;
-    case 2:
-      path = "/peri";
-      break;
-    case 3:
-      path = "/post";
-      break;
-    default:
-      path = "";
-  }
-  const { bookcaseInfo, isLoading } = useGetBookcase(`/book${path}`, navIndex);
+  useEffect(() => {
+    switch (navIndex) {
+      case 1:
+        setPath("/book/pre");
+        break;
+      case 2:
+        setPath("/book/peri");
+        break;
+      case 3:
+        setPath("/book/post");
+        break;
+      default:
+        setPath("/book");
+    }
+  }, [navIndex]);
+
+  const { bookcaseInfo, isLoading, isError } = useGetBookInfo(path);
 
   if (isLoading) {
     return <Loading />;
+  } else if (!bookcaseInfo || isError) {
+    return <Error404 />;
   } else if (bookcaseInfo.length === 0) {
     return (
       <StDefaultSection>
