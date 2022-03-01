1e273012c3e9efe90b233e03a9e552c3e421fbf4
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-03-02 01:50:40 +0900

    feat: MLFive bg

--- src/components/landing/MobileLandingFive.tsx
+++ src/components/landing/MobileLandingFive.tsx
@@ -1,23 +1,26 @@
-import React from "react";
-
 import { IcLandingMobileFour02 } from "../../assets/icons";
-import { StBackgroundFour, StH2Four, StParagraphFour, StWrapperFour } from "./MobileLandingFour";
+import { StMobileMain } from "../../pages/Landing";
+import { StBackgroundFour, StH2Four, StMobileBg, StParagraphFour, StWrapperFour } from "./MobileLandingFour";
 
 export default function MobileLandingFive() {
   return (
-    <StBackgroundFour>
-      <StWrapperFour>
-        <StH2Four>
-          읽을수록 쌓여가는 <br />
-          <strong>성장의 만족감을 느껴보세요.</strong>
-        </StH2Four>
-        <StParagraphFour>
-          나만의 북노트를 모아보고 내용을 재확인하며,
-          <br />
-          내가 읽은 책들을 모두 흡수해보세요.
-        </StParagraphFour>
-        <IcLandingMobileFour02 />
-      </StWrapperFour>
-    </StBackgroundFour>
+    <StMobileBg>
+      <StMobileMain>
+        <StBackgroundFour>
+          <StWrapperFour>
+            <StH2Four>
+              읽을수록 쌓여가는 <br />
+              <strong>성장의 만족감을 느껴보세요.</strong>
+            </StH2Four>
+            <StParagraphFour>
+              나만의 북노트를 모아보고 내용을 재확인하며,
+              <br />
+              내가 읽은 책들을 모두 흡수해보세요.
+            </StParagraphFour>
+            <IcLandingMobileFour02 />
+          </StWrapperFour>
+        </StBackgroundFour>
+      </StMobileMain>
+    </StMobileBg>
   );
 }
