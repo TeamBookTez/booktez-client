55caf89f25eab59717050a7e11c952c6795d1e92
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-03-03 13:18:26 +0900

    etc: 미사용 import 제거

--- src/components/common/PopUpDelete.tsx
+++ src/components/common/PopUpDelete.tsx
@@ -1,10 +1,8 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import { useSWRConfig } from "swr";
 
 import { ImgDeletePopUp } from "../../assets/images";
-import { navigatingBookInfoState } from "../../utils/atom";
 import { deleteData } from "../../utils/lib/api";
 import { StBtnCancel, StBtnDelete, StBtnWrapper, StDetail, StPopUp, StPopUpWrapper, StQuestion } from "./styled/PopUp";
 
