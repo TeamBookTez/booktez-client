0632d676737b7b76fc7e7d081a7a8d669a82f900
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-03-21 23:26:15 +0900

    [fix] drawer 오타 수정

--- src/components/bookNote/DrawerWrapper.tsx
+++ src/components/bookNote/DrawerWrapper.tsx
@@ -31,7 +31,7 @@ export default function DrawerWrapper(props: DrawerWrapperProps) {
     case 1:
       qaPair.question = "1. 이 책에 어떤 기대를 하고 계신가요?";
       qaPair.answer = [
-        "상황에 따라 변하는 '동기'를 한 곳에 잡아 두고 싶고,\n으로의 진행될 모든 업무에 대해 내가 이 일을 왜 하는지 명확하게 할 수 있는 힌트를 얻을 수 있다고 기대하고 있다.",
+        "상황에 따라 변하는 '동기'를 한 곳에 잡아 두고 싶고,\n앞으로 진행될 모든 업무에 대해 내가 이 일을 왜 하는지 명확하게 할 수 있는 힌트를 얻을 수 있다고 기대하고 있다.",
       ];
       break;
     case 2:
