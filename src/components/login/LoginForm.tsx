83ddb214f31e9f05934540ed5b15400942497de5
soryeongk <soryeongk@yolocorp.kr>
2022-04-21 15:49:00 +0900

    feat: useForm으로 이전 중

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 import { postData } from "../../utils/lib/api";
+import { FormData } from "../bookNote/periNote/PeriNote";
 import { AlertLabel, InputEmail, InputPwd } from "../common";
 import { Button } from "../common/styled/Button";
 
@@ -17,8 +19,10 @@ export default function LoginForm() {
   const [isPwdSight, setIsPwdSight] = useState<boolean>(false);
   const nav = useNavigate();
 
+  const { getValues, register, formState: { errors } } = useForm<FormData>();
+
   const postLogin = async () => {
-    if (isEmailEmpty || isPwdEmpty) return;
+    // if (isEmailEmpty || isPwdEmpty) return;
 
     const loginBody = {
       email,
@@ -77,31 +81,22 @@ export default function LoginForm() {
 
   return (
     <StForm onSubmit={handleSubmit}>
+
       <StLabel htmlFor="loginEmail">이메일</StLabel>
-      <InputEmail
-        whatPlaceholder="이메일을 입력해 주세요"
-        whatType="text"
-        whatId="loginEmail"
-        whatValue={email}
-        isEmpty={isEmailEmpty}
-        isError={isEmailError}
-        handleOnChange={handleOnChangeEmail}
-      />
-      <AlertLabel isError={isEmailError}>존재하지 않는 이메일 입니다.</AlertLabel>
+      <StInputEmail {...register("email", {
+        required: true,
+        pattern: /^(([^<>()\].,;:\s@"]+(\.[^<>()\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i,
+      })} placeholder="이메일을 입력해 주세요" />
+      {errors.email && <AlertLabel isError={true}>존재하지 않는 이메일 입니다.</AlertLabel>}
+
       <StLabelPwd htmlFor="loginPwd">비밀번호</StLabelPwd>
-      <InputPwd
-        whatPlaceholder="비밀번호를 입력해 주세요"
-        whatType={isPwdSight ? "text" : "password"}
-        whatId="loginPwd"
-        whatValue={pwd}
-        isEmpty={isPwdEmpty}
-        isError={isPwdError}
-        isPwdSight={isPwdSight}
-        toggleSightPwd={toggleSightPwd}
-        handleOnChange={handleOnChangePwd}
-      />
-      <AlertLabel isError={isPwdError}>비밀번호가 일치하지 않습니다.</AlertLabel>
-      <StLoginBtn active={!isEmailEmpty && !isPwdEmpty} onClick={postLogin}>
+      <StInputPwd {...register("password", {
+        required: true,
+        pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      })} placeholder="비밀번호를 입력해 주세요" />
+      {errors.password && <AlertLabel isError={true}>비밀번호가 일치하지 않습니다.</AlertLabel>}
+
+      <StLoginBtn active={true} type="submit">
         로그인
       </StLoginBtn>
     </StForm>
@@ -125,7 +120,7 @@ const StLabelPwd = styled(StLabel)`
   margin: 3.2rem 0 1.2rem;
 `;
 
-const StLoginBtn = styled(Button)<{ active: boolean }>`
+const StLoginBtn = styled(Button) <{ active: boolean }>`
   width: 46.4rem;
   height: 5.6rem;
 
@@ -145,4 +140,40 @@ const StLoginBtn = styled(Button)<{ active: boolean }>`
             cursor: default;
           }
         `}
+`;
+
+const StInputEmail = styled.input`
+  width: 100%;
+  height: 5.4rem;
+  padding-left: 2rem;
+
+  background-color: ${({ theme }) => theme.colors.white200};
+
+  border: 0.2rem solid ${({ theme }) => theme.colors.white200};
+  border-radius: 1rem;
+
+  ${({ theme }) => theme.fonts.body3}
+  color: ${({ theme }) => theme.colors.gray100};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray400};
+  }
+`;
+
+const StInputPwd = styled.input`
+  width: 100%;
+  height: 5.4rem;
+  padding-left: 2rem;
+
+  background-color: ${({ theme }) => theme.colors.white200};
+
+  border: 0.2rem solid ${({ theme }) => theme.colors.white200};
+  border-radius: 1rem;
+
+  ${({ theme }) => theme.fonts.body3}
+  color: ${({ theme }) => theme.colors.gray100};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray400};
+  }
 `;
