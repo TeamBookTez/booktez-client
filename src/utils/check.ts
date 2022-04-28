4f7d280811d28ed296f87ff99d018d40b5f55ed4
soryeongk <soryeongk.kr@gmail.com>
2022-04-28 23:49:52 +0900

    refactor: regex check.ts 파일로 분리

--- src/utils/check.ts
+++ src/utils/check.ts
@@ -29,3 +29,7 @@ export function checkPwdType(value: string) {
 
   return passwordRegex.test(value);
 }
+
+export const EMAIL_REGEX =
+  /^(([^<>()\].,;:\s@"]+(\.[^<>()\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+export const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
