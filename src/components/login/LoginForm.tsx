2586f16638ef2bd39e7c4c728e083b3139b4a1d9
soryeongk <soryeongk.kr@gmail.com>
2022-04-29 02:02:34 +0900

    refactor: error patterns 분리

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import styled, { css } from "styled-components";
 
-import { EMAIL_REGEX, INVALID_PWD_CHAR_LIST } from "../../utils/check";
+import { emailErrorPatterns, passwordErrorPatterns } from "../../utils/check";
 import { postData } from "../../utils/lib/api";
 import { FormData } from "../bookNote/periNote/PeriNote";
 import { AlertLabel } from "../common";
@@ -61,44 +61,13 @@ export default function LoginForm() {
   return (
     <StForm onSubmit={handleSubmit(submitForm)}>
       <StLabel htmlFor="loginEmail">이메일</StLabel>
-      <StInputEmail
-        {...register("email", {
-          required: { value: true, message: "이메일을 입력해주세요." },
-          pattern: {
-            value: EMAIL_REGEX,
-            message: "이메일 형식을 지켜주시기 바랍니다.",
-          },
-        })}
-        placeholder="이메일을 입력해 주세요"
-      />
+      <StInputEmail {...register("email", emailErrorPatterns)} placeholder="이메일을 입력해 주세요" />
       {errors.email?.message && <AlertLabel>{errors.email.message}</AlertLabel>}
 
       <StLabelPwd htmlFor="loginPwd">비밀번호</StLabelPwd>
       <StInputPwdWrapper>
         <StInputPwd
-          {...register("password", {
-            required: {
-              value: true,
-              message: "비밀번호를 입력해주세요.",
-            },
-            validate: (value: string) => {
-              if (value.match(/[,"'`]/)) {
-                const invalidChar = /[,"']/.exec(value);
-
-                if (invalidChar !== null) {
-                  return `${INVALID_PWD_CHAR_LIST[invalidChar[0]]}을 포함할 수 없습니다.`;
-                }
-              } else return true;
-            },
-            minLength: {
-              value: 8,
-              message: "비밀번호는 8자 이상 입력해주시기 바랍니다.",
-            },
-            maxLength: {
-              value: 64,
-              message: "비밀번호는 64자 이하 입력해주시기 바랍니다.",
-            },
-          })}
+          {...register("password", passwordErrorPatterns)}
           placeholder="비밀번호를 입력해 주세요"
           type={isPwdSight ? "text" : "password"}
         />
