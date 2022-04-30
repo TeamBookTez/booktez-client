d9abdc0cf9cab8f9ec10da857177d06c62cbf169
soryeongk <soryeongk.kr@gmail.com>
2022-04-30 22:18:28 +0900

    refactor: 회원가입 이메일 폼 useForm으로 교체 - 중복은 추후 제거

--- src/components/signup/FirstStep.tsx
+++ src/components/signup/FirstStep.tsx
@@ -1,65 +1,69 @@
+import axios from "axios";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 import { IcSignupChecking } from "../../assets/icons";
 import { ImgSignupFirst } from "../../assets/images";
 import { UserData } from "../../pages/Signup";
-import { checkEmailType } from "../../utils/check";
-import { getData } from "../../utils/lib/api";
+import { checkEmailType, emailErrorPatterns } from "../../utils/check";
+import { getData, postData } from "../../utils/lib/api";
+import { FormData } from "../bookNote/periNote/PeriNote";
 import { AlertLabel, InputEmail } from "../common";
 import { Button } from "../common/styled/Button";
 import { LabelHidden } from "../common/styled/LabelHidden";
 import { StSignupHeading2, StSignupImage, StSignupParagraph } from "../common/styled/Signup";
+import { StInputEmail } from "../login/LoginForm";
 
 export default function FirstStep() {
   const [userData, setUserData] = useOutletContext<[UserData, React.Dispatch<React.SetStateAction<UserData>>]>();
-  const [email, setEmail] = useState<string>("");
-  const [isEmailEmpty, setIsEmailEmpty] = useState<boolean>(true);
-  const [isEmailError, setIsEmailError] = useState<boolean>(false);
   const [isAgreeCondition, setIsAgreeCondition] = useState<boolean>(false);
-  const [errorMessage, setErrorMessage] = useState<string>("");
-  const [isEmailValid, setIsEmailValid] = useState<boolean>(true);
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    getEmail(userData["email"]);
-  }, [userData]);
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm<FormData>({
+    mode: "onSubmit",
+  });
 
-  const getEmail = async (emailData: string) => {
-    const res = await getData(`/auth/email?email=${emailData}`);
-    const resData = res.data;
+  const submitForm = async (loginFormData: FormData) => {
+    const { email } = loginFormData;
+    let errorMessage;
 
-    setIsEmailValid(resData.data.isValid);
-    setErrorMessage(resData.message);
-  };
+    try {
+      const {
+        data: { data },
+      } = await getData(`/auth/email?email=${email}`);
 
-  const goNextStep = () => {
-    if (isEmailEmpty) return;
-    if (!checkEmailType(userData["email"]) || isEmailValid === false) {
-      return setIsEmailError(true);
+      if (data.isValid) {
+        if (isAgreeCondition) {
+          setUserData((current) => ({ ...current, email }));
+          navigate("/signup/2", { state: "rightpath" });
+        } else {
+          errorMessage = "개인정보 수집 및 이용 약관에 동의해주시기 바랍니다.";
+        }
+      } else {
+        errorMessage = data.message;
+      }
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        errorMessage = err.response?.data.message;
+      }
     }
 
-    navigate("/signup/2", { state: "rightpath" });
+    if (errorMessage) {
+      setError("email", {
+        type: "server",
+        message: errorMessage,
+      });
+    }
   };
-
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const targetValue = e.target.value;
-
-    setIsEmailError(false);
-    setIsEmailEmpty(targetValue === "");
-    setEmail(targetValue);
-    setUserData((current) => ({ ...current, email: targetValue }));
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    goNextStep();
-  };
-
-  const isSubmitDisabled = isEmailEmpty || isEmailError || !isAgreeCondition;
 
   return (
     <motion.div
@@ -73,18 +77,12 @@ export default function FirstStep() {
       <StSignupImage src={ImgSignupFirst} alt="회원가입 첫 단계" />
       <StSignupHeading2>나만의 서재를 만드는 중이에요!</StSignupHeading2>
       <StSignupParagraph>당신의 이메일을 입력해 주세요.</StSignupParagraph>
-      <StForm onSubmit={handleSubmit}>
+
+      <StForm onSubmit={handleSubmit(submitForm)}>
         <LabelHidden htmlFor="signupEmail">이메일</LabelHidden>
-        <InputEmail
-          whatPlaceholder="이메일을 입력해 주세요"
-          whatType="text"
-          whatId="signupEmail"
-          whatValue={email}
-          isEmpty={isEmailEmpty}
-          isError={isEmailError}
-          handleOnChange={handleOnChange}
-        />
-        {/* <AlertLabel isError={isEmailError}>{errorMessage}</AlertLabel> */}
+        <StInputEmail {...register("email", emailErrorPatterns)} placeholder="이메일을 입력해 주세요" />
+        {errors.email?.message && <AlertLabel>{errors.email.message}</AlertLabel>}
+
         <StAgreeConditionBox htmlFor="signupAgree" onClick={() => setIsAgreeCondition((prev) => !prev)}>
           <StIcSignupChecking isagree={isAgreeCondition} />
           <p>
@@ -96,7 +94,8 @@ export default function FirstStep() {
             에 동의합니다.
           </p>
         </StAgreeConditionBox>
-        <StNextStepBtn disabled={isSubmitDisabled} onClick={goNextStep}>
+
+        <StNextStepBtn disabled={!isValid} type="submit">
           다음 계단
         </StNextStepBtn>
       </StForm>
